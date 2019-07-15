a64726aaa001604f9442bd26b345d64c7b248a25
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-07-15 14:42:45 +0000

    dark/light theme using vuex

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  }
+};
+</script>
+
+
 <style lang="scss">
 body {
   background: $dark-blue;
--- src/store.js
+++ src/store.js
@@ -3,8 +3,37 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  window.localStorage.setItem("isDarkMode", "true");
+}
+const userSelectedDarkMode =
+  window.localStorage.getItem("isDarkMode") === "true";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    isDarkMode: userSelectedDarkMode
+  },
+  getters: {
+    isDarkMode(state) {
+      return state.isDarkMode;
+    }
+  },
+  mutations: {
+    toggleDarkMode(state) {
+      if (state.isDarkMode === true) {
+        state.isDarkMode = false;
+        document.body.style.background = "#f0f3f5";
+        window.localStorage.setItem("isDarkMode", "false");
+      } else {
+        state.isDarkMode = true;
+        document.body.style.background = "#212c4f";
+        window.localStorage.setItem("isDarkMode", "true");
+      }
+    }
+  },
+  actions: {
+    triggerDarkMode({ commit }) {
+      commit("toggleDarkMode");
+    }
+  }
 });
